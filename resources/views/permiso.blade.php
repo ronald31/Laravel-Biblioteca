83abc3d81b737faebdfae044745355b1ba5a6ab5
tutorial <ronaldat32@hotmail.com>
2019-08-24 23:47:20 -0500

    rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,1 @@
+Hola men .{{$nombre}} y {{$slug}}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('permiso/{nombre}/{slug?}','PermisoController@index');
+
+//Route::view('permiso', 'permiso');
